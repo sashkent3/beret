a479981f32911da3b66e288085b6c7d962512a87
uliana57 <uliana.chekina@gmail.com>
2019-11-12 13:57:22 +0300

    Added authors info

--- lib/info.dart
+++ lib/info.dart
@@ -10,9 +10,89 @@ class Info extends StatelessWidget {
                 title: Text('Шляпа'),
                 bottom: TabBar(
                     tabs: <Widget>[Tab(text: 'Правила'), Tab(text: 'Авторы')])),
-            body: TabBarView(children: [
-              Text('Скоро здесь появятся правила!'),
-              Text('Скоро здесь появится информация об авторах!')
-            ])));
+            body: Padding(
+                padding: EdgeInsets.all(12),
+                child: TabBarView(children: [
+                  Text('Скоро здесь появятся правила!'),
+                  SingleChildScrollView(
+                      child: RichText(
+                          text: TextSpan(
+                              style: TextStyle(color: Colors.black),
+                              children: [
+                                TextSpan(
+                                    text: 'Новый Android-клиент:\n',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold)),
+                                TextSpan(text: 'Зинов Александр\n'),
+                                TextSpan(
+                                    text: 'под руководством Зинова Николая\n\n'),
+                                TextSpan(
+                                    text: 'Старый Android-клиент: ',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold)),
+                                TextSpan(text: 'Денис Кузнецов\n\n'),
+                                TextSpan(
+                                    text: 'Сервер: ',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold)),
+                                TextSpan(
+                                    text:
+                                    'ЛКШ.Зима 2013-2014, Параллель П в составе:\n\n'),
+                                TextSpan(
+                                    text: 'Преподаватели:\n',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold)),
+                                TextSpan(text: 'Дробин Вадим Дмитриевич\n'),
+                                TextSpan(text: 'Кузнецов Денис Васильевич\n\n'),
+                                TextSpan(
+                                    text: 'Школьники:\n',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold)),
+                                TextSpan(text: 'Калинин Николай\n'),
+                                TextSpan(text: 'Козловцев Константин\n'),
+                                TextSpan(text: 'Лабутин Игорь\n'),
+                                TextSpan(text: 'Павлов Иван\n'),
+                                TextSpan(text: 'Торба Дмитрий\n\n'),
+                                TextSpan(
+                                    text: 'Последующая работа над сервером:\n\n',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold)),
+                                TextSpan(text: 'Зинов Николай\n'),
+                                TextSpan(text: 'Павлов Иван\n'),
+                                TextSpan(text: 'под руководством\n'),
+                                TextSpan(
+                                    text: 'Гольдштейна Виталия Борисовича\n'),
+                                TextSpan(
+                                    text: 'Кузнецова Дениса Васильевича\n\n'),
+                                TextSpan(
+                                    text:
+                                    'Разработка основана на приложении, написанном участниками параллели П в ЛКШ.Август 2013:\n\n'),
+                                TextSpan(
+                                    text: 'Преподаватели\n',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold)),
+                                TextSpan(
+                                    text: 'Гольдштейн Виталий Борисович\n'),
+                                TextSpan(text: 'Дробин Вадим Дмитриевич\n'),
+                                TextSpan(text: 'Кузнецов Денис Васильевич\n'),
+                                TextSpan(text: 'Назаров Сергей Игоревич\n'),
+                                TextSpan(
+                                    text: 'Шестимеров Андрей Алексеевич\n\n'),
+                                TextSpan(
+                                    text: 'Школьники:\n',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold)),
+                                TextSpan(text: 'Зинов Николай\n'),
+                                TextSpan(text: 'Калинин Николай\n'),
+                                TextSpan(text: 'Карпенко Даниил\n'),
+                                TextSpan(text: 'Козловцев Константин\n'),
+                                TextSpan(text: 'Кравченко Дмитрий\n'),
+                                TextSpan(text: 'Лабутин Игорь\n'),
+                                TextSpan(text: 'Лиференко Даниил\n'),
+                                TextSpan(text: 'Павлов Иван\n'),
+                                TextSpan(text: 'Сергеев Иван\n'),
+                                TextSpan(text: 'Торба Дмитрий')
+                              ])))
+                ]))));
   }
 }
