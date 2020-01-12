4bfcdc0c5b832a37095a44f81df9df55a04df50e
sasha <sashkent3@gmail.com>
2020-01-12 18:39:28 +0300

    Commiting generated part

---
+++ lib/deathmatch_state.g.dart
@@ -0,0 +1,353 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'deathmatch_state.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$DeathmatchState on _DeathmatchState, Store {
+  final _$diffAndTimeVisibilityAtom =
+      Atom(name: '_DeathmatchState.diffAndTimeVisibility');
+
+  @override
+  ObservableList<bool> get diffAndTimeVisibility {
+    _$diffAndTimeVisibilityAtom.context
+        .enforceReadPolicy(_$diffAndTimeVisibilityAtom);
+    _$diffAndTimeVisibilityAtom.reportObserved();
+    return super.diffAndTimeVisibility;
+  }
+
+  @override
+  set diffAndTimeVisibility(ObservableList<bool> value) {
+    _$diffAndTimeVisibilityAtom.context.conditionallyRunInAction(() {
+      super.diffAndTimeVisibility = value;
+      _$diffAndTimeVisibilityAtom.reportChanged();
+    }, _$diffAndTimeVisibilityAtom,
+        name: '${_$diffAndTimeVisibilityAtom.name}_set');
+  }
+
+  final _$audioPlayerAtom = Atom(name: '_DeathmatchState.audioPlayer');
+
+  @override
+  AudioCache get audioPlayer {
+    _$audioPlayerAtom.context.enforceReadPolicy(_$audioPlayerAtom);
+    _$audioPlayerAtom.reportObserved();
+    return super.audioPlayer;
+  }
+
+  @override
+  set audioPlayer(AudioCache value) {
+    _$audioPlayerAtom.context.conditionallyRunInAction(() {
+      super.audioPlayer = value;
+      _$audioPlayerAtom.reportChanged();
+    }, _$audioPlayerAtom, name: '${_$audioPlayerAtom.name}_set');
+  }
+
+  final _$dictionaryAtom = Atom(name: '_DeathmatchState.dictionary');
+
+  @override
+  Dictionary get dictionary {
+    _$dictionaryAtom.context.enforceReadPolicy(_$dictionaryAtom);
+    _$dictionaryAtom.reportObserved();
+    return super.dictionary;
+  }
+
+  @override
+  set dictionary(Dictionary value) {
+    _$dictionaryAtom.context.conditionallyRunInAction(() {
+      super.dictionary = value;
+      _$dictionaryAtom.reportChanged();
+    }, _$dictionaryAtom, name: '${_$dictionaryAtom.name}_set');
+  }
+
+  final _$rngAtom = Atom(name: '_DeathmatchState.rng');
+
+  @override
+  Random get rng {
+    _$rngAtom.context.enforceReadPolicy(_$rngAtom);
+    _$rngAtom.reportObserved();
+    return super.rng;
+  }
+
+  @override
+  set rng(Random value) {
+    _$rngAtom.context.conditionallyRunInAction(() {
+      super.rng = value;
+      _$rngAtom.reportChanged();
+    }, _$rngAtom, name: '${_$rngAtom.name}_set');
+  }
+
+  final _$wordAtom = Atom(name: '_DeathmatchState.word');
+
+  @override
+  String get word {
+    _$wordAtom.context.enforceReadPolicy(_$wordAtom);
+    _$wordAtom.reportObserved();
+    return super.word;
+  }
+
+  @override
+  set word(String value) {
+    _$wordAtom.context.conditionallyRunInAction(() {
+      super.word = value;
+      _$wordAtom.reportChanged();
+    }, _$wordAtom, name: '${_$wordAtom.name}_set');
+  }
+
+  final _$difficultyAtom = Atom(name: '_DeathmatchState.difficulty');
+
+  @override
+  int get difficulty {
+    _$difficultyAtom.context.enforceReadPolicy(_$difficultyAtom);
+    _$difficultyAtom.reportObserved();
+    return super.difficulty;
+  }
+
+  @override
+  set difficulty(int value) {
+    _$difficultyAtom.context.conditionallyRunInAction(() {
+      super.difficulty = value;
+      _$difficultyAtom.reportChanged();
+    }, _$difficultyAtom, name: '${_$difficultyAtom.name}_set');
+  }
+
+  final _$additionalTimeAtom = Atom(name: '_DeathmatchState.additionalTime');
+
+  @override
+  int get additionalTime {
+    _$additionalTimeAtom.context.enforceReadPolicy(_$additionalTimeAtom);
+    _$additionalTimeAtom.reportObserved();
+    return super.additionalTime;
+  }
+
+  @override
+  set additionalTime(int value) {
+    _$additionalTimeAtom.context.conditionallyRunInAction(() {
+      super.additionalTime = value;
+      _$additionalTimeAtom.reportChanged();
+    }, _$additionalTimeAtom, name: '${_$additionalTimeAtom.name}_set');
+  }
+
+  final _$partnerAtom = Atom(name: '_DeathmatchState.partner');
+
+  @override
+  String get partner {
+    _$partnerAtom.context.enforceReadPolicy(_$partnerAtom);
+    _$partnerAtom.reportObserved();
+    return super.partner;
+  }
+
+  @override
+  set partner(String value) {
+    _$partnerAtom.context.conditionallyRunInAction(() {
+      super.partner = value;
+      _$partnerAtom.reportChanged();
+    }, _$partnerAtom, name: '${_$partnerAtom.name}_set');
+  }
+
+  final _$scoreAtom = Atom(name: '_DeathmatchState.score');
+
+  @override
+  int get score {
+    _$scoreAtom.context.enforceReadPolicy(_$scoreAtom);
+    _$scoreAtom.reportObserved();
+    return super.score;
+  }
+
+  @override
+  set score(int value) {
+    _$scoreAtom.context.conditionallyRunInAction(() {
+      super.score = value;
+      _$scoreAtom.reportChanged();
+    }, _$scoreAtom, name: '${_$scoreAtom.name}_set');
+  }
+
+  final _$mainTimerAtom = Atom(name: '_DeathmatchState.mainTimer');
+
+  @override
+  int get mainTimer {
+    _$mainTimerAtom.context.enforceReadPolicy(_$mainTimerAtom);
+    _$mainTimerAtom.reportObserved();
+    return super.mainTimer;
+  }
+
+  @override
+  set mainTimer(int value) {
+    _$mainTimerAtom.context.conditionallyRunInAction(() {
+      super.mainTimer = value;
+      _$mainTimerAtom.reportChanged();
+    }, _$mainTimerAtom, name: '${_$mainTimerAtom.name}_set');
+  }
+
+  final _$addTimerAtom = Atom(name: '_DeathmatchState.addTimer');
+
+  @override
+  int get addTimer {
+    _$addTimerAtom.context.enforceReadPolicy(_$addTimerAtom);
+    _$addTimerAtom.reportObserved();
+    return super.addTimer;
+  }
+
+  @override
+  set addTimer(int value) {
+    _$addTimerAtom.context.conditionallyRunInAction(() {
+      super.addTimer = value;
+      _$addTimerAtom.reportChanged();
+    }, _$addTimerAtom, name: '${_$addTimerAtom.name}_set');
+  }
+
+  final _$stateAtom = Atom(name: '_DeathmatchState.state');
+
+  @override
+  String get state {
+    _$stateAtom.context.enforceReadPolicy(_$stateAtom);
+    _$stateAtom.reportObserved();
+    return super.state;
+  }
+
+  @override
+  set state(String value) {
+    _$stateAtom.context.conditionallyRunInAction(() {
+      super.state = value;
+      _$stateAtom.reportChanged();
+    }, _$stateAtom, name: '${_$stateAtom.name}_set');
+  }
+
+  final _$matchTimerAtom = Atom(name: '_DeathmatchState.matchTimer');
+
+  @override
+  Timer get matchTimer {
+    _$matchTimerAtom.context.enforceReadPolicy(_$matchTimerAtom);
+    _$matchTimerAtom.reportObserved();
+    return super.matchTimer;
+  }
+
+  @override
+  set matchTimer(Timer value) {
+    _$matchTimerAtom.context.conditionallyRunInAction(() {
+      super.matchTimer = value;
+      _$matchTimerAtom.reportChanged();
+    }, _$matchTimerAtom, name: '${_$matchTimerAtom.name}_set');
+  }
+
+  final _$startingCountdownAtom =
+      Atom(name: '_DeathmatchState.startingCountdown');
+
+  @override
+  int get startingCountdown {
+    _$startingCountdownAtom.context.enforceReadPolicy(_$startingCountdownAtom);
+    _$startingCountdownAtom.reportObserved();
+    return super.startingCountdown;
+  }
+
+  @override
+  set startingCountdown(int value) {
+    _$startingCountdownAtom.context.conditionallyRunInAction(() {
+      super.startingCountdown = value;
+      _$startingCountdownAtom.reportChanged();
+    }, _$startingCountdownAtom, name: '${_$startingCountdownAtom.name}_set');
+  }
+
+  final _$countdownTimerAtom = Atom(name: '_DeathmatchState.countdownTimer');
+
+  @override
+  Timer get countdownTimer {
+    _$countdownTimerAtom.context.enforceReadPolicy(_$countdownTimerAtom);
+    _$countdownTimerAtom.reportObserved();
+    return super.countdownTimer;
+  }
+
+  @override
+  set countdownTimer(Timer value) {
+    _$countdownTimerAtom.context.conditionallyRunInAction(() {
+      super.countdownTimer = value;
+      _$countdownTimerAtom.reportChanged();
+    }, _$countdownTimerAtom, name: '${_$countdownTimerAtom.name}_set');
+  }
+
+  final _$_DeathmatchStateActionController =
+      ActionController(name: '_DeathmatchState');
+
+  @override
+  void toggleVisibility(int idx) {
+    final _$actionInfo = _$_DeathmatchStateActionController.startAction();
+    try {
+      return super.toggleVisibility(idx);
+    } finally {
+      _$_DeathmatchStateActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void startBlinking(int idx) {
+    final _$actionInfo = _$_DeathmatchStateActionController.startAction();
+    try {
+      return super.startBlinking(idx);
+    } finally {
+      _$_DeathmatchStateActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void guessedRight() {
+    final _$actionInfo = _$_DeathmatchStateActionController.startAction();
+    try {
+      return super.guessedRight();
+    } finally {
+      _$_DeathmatchStateActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void timerTick() {
+    final _$actionInfo = _$_DeathmatchStateActionController.startAction();
+    try {
+      return super.timerTick();
+    } finally {
+      _$_DeathmatchStateActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setState(String newState) {
+    final _$actionInfo = _$_DeathmatchStateActionController.startAction();
+    try {
+      return super.setState(newState);
+    } finally {
+      _$_DeathmatchStateActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void startMatch() {
+    final _$actionInfo = _$_DeathmatchStateActionController.startAction();
+    try {
+      return super.startMatch();
+    } finally {
+      _$_DeathmatchStateActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void startingCountdownTick() {
+    final _$actionInfo = _$_DeathmatchStateActionController.startAction();
+    try {
+      return super.startingCountdownTick();
+    } finally {
+      _$_DeathmatchStateActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void startCountdown() {
+    final _$actionInfo = _$_DeathmatchStateActionController.startAction();
+    try {
+      return super.startCountdown();
+    } finally {
+      _$_DeathmatchStateActionController.endAction(_$actionInfo);
+    }
+  }
+}
